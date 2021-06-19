462a367dee1d76685082f8b7206ebd7f1be0775a
Bernard Bartholome <bernard@bartholome.be>
2021-06-19 23:13:28 +0200

    merge ppt and video in main2

--- test vlc.py
+++ test vlc.py
@@ -7,6 +7,8 @@ def video(source):
     # creating a vlc instance
     vlc_instance = vlc.Instance()
 
+    media = vlc_instance.media_new("death_note.mkv")
+
     # creating a media player
     player = vlc_instance.media_player_new()
 
@@ -16,31 +18,30 @@ def video(source):
     # setting media to the player
     player.set_media(media)
 
-
-
     # play the video
 
     player.play()
     time.sleep(0.2)
-  #  player.pause()
-  #  time.sleep(0.5)
-  #  player.play()
-    # wait time
+    player.pause()
 
+    # wait time
+    time.sleep(1)
+    player.play()
     # focus and maximize the vlc player
-    handle = win32gui.FindWindow(None, "VLC (Direct3D11 output)")
-    win32gui.SetForegroundWindow(handle)
-    win32gui.ShowWindow(handle, win32con.SW_MAXIMIZE)
+    try:
+        handle = win32gui.FindWindow(None, "VLC (Direct3D11 output)")
+        win32gui.SetForegroundWindow(handle)
+        win32gui.ShowWindow(handle, win32con.SW_MAXIMIZE)
+    except:
+        print ('windows not found')
+
     #getting the duration of the video
     duration = player.get_length()
 
     # printing the duration of the video
-    print("Duration : " + str(duration))
-    player.play()
+    # print("Duration : " + str(duration))
 
-
-
-    # wait time
+    # wait video time time
     time.sleep(duration/1000)
 
 
